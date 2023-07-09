d97b6748578a5d14a54b48be3d382565df29348d
gustavo <gsarruda.office@gmail.com>
2023-07-09 19:52:58 -0300

    começando a refatoracao do timer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import Sounds from "./sounds.js";
 
 import soundEvents from "./sound-events.js";
 
+import Timer from "./timer.js";
+
 import {
   minutesDisplay,
   secondsDisplay,
@@ -20,6 +22,8 @@ import {
 const sounds = Sounds();
 
 const events = soundEvents({ sounds });
+
+const timer = Timer(minutesDisplay, secondsDisplay);
 
 forestButtom.addEventListener("click", () => {
   events.songButtonEvent("forestSound", forestButtom);
@@ -43,4 +47,28 @@ fireplaceButtom.addEventListener("click", () => {
   events.songButtonEvent("fireplaceSound", fireplaceButtom);
   fireplaceButtom.classList.toggle("selected");
   events.unselectAndRemoveOthers("fireplaceSound", fireplaceButtom);
+});
+
+playButtom.addEventListener("click", () => {
+  timer.countdown();
+});
+
+stopButtom.addEventListener("click", () => {
+  timer.hold();
+});
+
+addTimeButtom.addEventListener("click", () => {
+  minutesDisplay.textContent = String(
+    Number(minutesDisplay.textContent) + 5
+  ).padStart(2, "0");
+});
+
+subTimeButton.addEventListener("click", () => {
+  if (!(Number(minutesDisplay.textContent) - 5 < 0)) {
+    minutesDisplay.textContent = String(
+      Number(minutesDisplay.textContent) - 5
+    ).padStart(2, "0");
+  } else {
+    timer.updateTimerDisplay(0, 0);
+  }
 });
---
+++ js/timer.js
@@ -0,0 +1,37 @@
+export default function Timer({ minutesDisplay, secondsDisplay }) {
+  let timerTimeOut;
+
+  function countdown() {
+    timerTimeOut = setTimeout(function () {
+      let minutes = Number(minutesDisplay.textContent);
+      let seconds = Number(secondsDisplay.textContent);
+
+      if (minutes <= 0 && seconds <= 0) {
+        return;
+      }
+
+      if (seconds <= 0) {
+        seconds = 60;
+        --minutes;
+      }
+
+      updateTimerDisplay(minutes, String(seconds - 1));
+      countdown();
+    }, 100);
+  }
+
+  function updateTimerDisplay(minutes, seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2, "0");
+    secondsDisplay.textContent = String(seconds).padStart(2, "0");
+  }
+
+  function hold() {
+    clearTimeout(timerTimeOut);
+  }
+
+  return {
+    countdown,
+    updateTimerDisplay,
+    hold,
+  };
+}
